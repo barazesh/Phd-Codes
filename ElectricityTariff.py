93c602f32913852802cca85c6d7e76dd824b47d8
barazesh <barazesh.mr90@gmail.com>
2023-12-03 14:27:36 +0100

    fixed electricity tariff history

--- ElectricityTariff.py
+++ ElectricityTariff.py
@@ -1,41 +1,39 @@
 class ElectricityTariff:
     def __init__(self, initialFixedTariff: float,initialVariableTariff: float) -> None:
-        self.__history = [{'time':0, 'fixed':initialFixedTariff,'variable':initialVariableTariff}]
+        self._history = [{'time':0, 'fixed':initialFixedTariff,'variable':initialVariableTariff}]
         self.currentFixedPrice = initialFixedTariff
         self.currentVariablePrice = initialVariableTariff
 
-    def ChangeTariff(self, time:int, variablePriceChange:float, fixedPriceChange:float) -> None:
-        temp={
-            'time':time,
-            'fixed':self.currentFixedPrice,
-            'variable':self.currentVariablePrice
-        }
-        self.__history.append(temp)
+    def SetTariffbyChange(self, time:int, variablePriceChange:float, fixedPriceChange:float) -> None:
         self.currentFixedPrice += fixedPriceChange
         if self.currentFixedPrice < 0:
             self.currentFixedPrice=0
         self.currentVariablePrice += variablePriceChange
         if self.currentVariablePrice < 0:
             self.currentVariablePrice=0
+        self.AppendHistory(time)
 
-    def SetNewTariff(self, time:int, new_variablePrice:float, new_fixedPrice:float) -> None:
-        temp={
-            'time':time,
-            'fixed':self.currentFixedPrice,
-            'variable':self.currentVariablePrice
-        }
-        self.__history.append(temp)
+    def SetTariffbvNewValue(self, time:int, new_variablePrice:float, new_fixedPrice:float) -> None:
         self.currentFixedPrice = new_fixedPrice
         if self.currentFixedPrice < 0:
             self.currentFixedPrice=0
         self.currentVariablePrice = new_variablePrice
         if self.currentVariablePrice < 0:
             self.currentVariablePrice=0
+        self.AppendHistory(time)
+
+    def AppendHistory(self, time):
+        temp={
+            'time':time,
+            'fixed':self.currentFixedPrice,
+            'variable':self.currentVariablePrice
+        }
+        self._history.append(temp)
 
     def GetPriceChangeValue(self) -> dict:
         result={
-            'fixed':self.currentFixedPrice-self.__history[-1]['fixed'],
-            'variable':self.currentVariablePrice-self.__history[-1]['variable']
+            'fixed':self._history[-1]['fixed']-self._history[-2]['fixed'],
+            'variable':self._history[-1]['variable']-self._history[-2]['variable']
         }
         return result
 
@@ -43,11 +41,11 @@ class ElectricityTariff:
         priceChange=self.GetPriceChangeValue()
         result={}
         for k in priceChange.keys():
-            if self.__history[-1][k] == 0:
+            if self._history[-2][k] == 0:
                 result[k]=priceChange[k]/ 1e-5
             else:
-                result[k]=priceChange[k]/ self.__history[-1][k]
+                result[k]=priceChange[k]/ self._history[-2][k]
         return result
 
     def GetHistory(self):
-        return self.__history
+        return self._history
